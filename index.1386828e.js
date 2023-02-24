ad12032b846260bd6f740b3f66aed005856bb06d
MishaStryzhka <misha.strizhcko@gmail.com>
2023-02-24 21:37:14 +0000

    Deploying to gh-pages from @ MishaStryzhka/goit-js-hw-10@d72aba40085d4731c7172d1a5f0b54401d7d77d1 🚀

--- index.1386828e.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("search-box").addEventListener("input",(function(e){var n,t;(n=e.currentTarget.value,t="https://restcountries.com/v2/name/".concat(n,"?fields=name,"),fetch(t).then((function(e){return e.json()}))).then((function(e){return console.log(e)}))}));
-//# sourceMappingURL=index.1386828e.js.map
--- index.a04ad807.js
+++
@@ -1,2 +0,0 @@
-({input:document.getElementById("search-box")}).input.addEventListener("input",(e=>{var t;(t=e.currentTarget.value,fetch(`https://restcountries.com/v2/name/${t}?fields=name,`).then((e=>e.json()))).then((e=>console.log(e)))}));
-//# sourceMappingURL=index.a04ad807.js.map
